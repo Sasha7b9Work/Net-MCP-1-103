cdb3b3162203f3d98a9265730020974e81382416
Sasha7b9Work <Phuntik-it@tut.by>
2024-06-10 10:36:36 +0300

    Добавлены файлы

---
+++ sources/common/_VS_KEIL/std_includes/math.h
@@ -0,0 +1,13 @@
+#pragma once
+
+float fabsf(float);
+float fabs(double);
+float sqrtf(float);
+float log10f(float);
+float log(float);
+float cosf(float);
+float sinf(float);
+bool isnan(float);
+float expf(float);
+void srand(uint);
+float powf(float, float);
